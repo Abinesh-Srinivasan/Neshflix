1ccfad8037878a654b142e5e7afc61c6cf69ff30
Abinesh-Srinivasan <abineshsrinivasan1301@gmail.com>
2024-08-25 15:41:12 +0530

    Deployment functionality added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -12,6 +13,7 @@ import connectDb from "./config/dataBase.js";
 
 const app = express();
 const { PORT } = ENV_VARS;
+const __dirname = path.resolve();
 
 app.use(express.json()); // to parse req.body
 app.use(cookieParser());
@@ -21,6 +23,14 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+
+if (ENV_VARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening to the port ${PORT}...`);
