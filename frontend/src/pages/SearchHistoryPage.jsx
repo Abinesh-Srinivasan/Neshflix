0aa5ab092e6526c8c7e23c61f601b3fb65281f75
Abinesh-Srinivasan <abineshsrinivasan1301@gmail.com>
2024-08-25 11:38:46 +0530

    SearchHistoryPage responsiveness completed

--- frontend/src/pages/SearchHistoryPage.jsx
+++ frontend/src/pages/SearchHistoryPage.jsx
@@ -64,10 +64,10 @@ const SearchHistoryPage = () => {
     return (
       <div className="bg-black min-h-screen text-white">
         <Navbar />
-        <div className="max-w-6xl mx-auto px-4 py-8">
-          <h1 className="text-3xl font-bold mb-8">Search History</h1>
+        <div className="max-w-6xl mx-auto px-4 py-8 2xl:mt-14 2xl:max-w-screen-2xl 4k:max-w-screen-4k 4k:mx-72 4k:mt-56">
+          <h1 className="text-3xl font-bold mb-8 2xl:text-5xl 4k:text-8xl">Search History</h1>
           <div className="flex justify-center items-center h-96">
-            <p className="text-xl">No search history found</p>
+            <p className="text-xl 2xl:text-3xl 4k:text-7xl">No search history found</p>
           </div>
         </div>
       </div>
@@ -75,31 +75,31 @@ const SearchHistoryPage = () => {
   }
 
   return (
-    <div className="bg-black text-white min-h-screen">
+    <div className="bg-black text-white min-h-screen 4k:pb-20">
       <Navbar />
 
-      <div className="max-w-6xl mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-8">Search History</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3  gap-4">
+      <div className="max-w-6xl mx-auto px-4 py-8 2xl:mt-16 2xl:max-w-screen-2xl 4k:max-w-screen-4k 4k:mx-56 4k:mt-48">
+        <h1 className="text-3xl font-bold mb-8 2xl:text-5xl 2xl:mb-12 4k:text-8xl 4k:mb-24">Search History</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3  gap-4 2xl:gap-8 4k:gap-16">
           {searchHistory?.map((entry) => (
             <div
               key={entry.id}
-              className="bg-gray-800 p-4 rounded flex items-start"
+              className="bg-gray-800 p-4 rounded flex items-start 2xl:p-6 2xl:rounded-lg 4k:p-16 4k:rounded-3xl"
             >
               <img
                 src={SMALL_IMG_BASE_URL + entry.image}
                 alt="History image"
-                className="size-16 rounded-full object-cover mr-4"
+                className="size-16 rounded-full object-cover mr-4 2xl:size-24 2xl:mr-6 4k:size-56"
               />
-              <div className="flex flex-col">
-                <span className="text-white text-lg">{entry.title}</span>
-                <span className="text-gray-400 text-sm">
+              <div className="flex flex-col 4k:gap-6">
+                <span className="text-white text-lg 2xl:text-xl 4k:text-6xl">{entry.title}</span>
+                <span className="text-gray-400 text-sm 2xl:text-lg 4k:text-5xl">
                   {formatDate(entry.createdAt)}
                 </span>
               </div>
 
               <span
-                className={`py-1 px-3 min-w-20 text-center rounded-full text-sm  ml-auto ${
+                className={`py-1 px-3 min-w-20 text-center rounded-full text-sm  ml-auto 2xl:text-lg 2xl:px-6 2xl:py-2 4k:text-6xl 4k:py-4 4k:px-12 ${
                   entry.searchType === "movie"
                     ? "bg-red-600"
                     : entry.searchType === "tv"
@@ -110,7 +110,7 @@ const SearchHistoryPage = () => {
                 {entry.searchType[0].toUpperCase() + entry.searchType.slice(1)}
               </span>
               <Trash
-                className="size-5 ml-4 cursor-pointer hover:fill-red-600 hover:text-red-600"
+                className="size-5 ml-4 cursor-pointer hover:fill-red-600 hover:text-red-600 2xl:size-8 2xl:ml-8 4k:size-20 4k:ml-16"
                 onClick={() => handleDelete(entry)}
               />
             </div>
